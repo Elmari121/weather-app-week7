df4a01056892e7311a5b32990350b233492e6cd7
Elmari121 <robbertselmari@gmail.com>
2025-03-16 15:50:25 +0200

    js update

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function refreshWeather(weatherData) {
   let iconElement = document.querySelector("#icon");
   let date = new Date(weatherData.data.time * 1000);
 
-  // Update the UI with the weather data
   cityElement.innerHTML = weatherData.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = weatherData.data.condition.description;
@@ -17,6 +16,8 @@ function refreshWeather(weatherData) {
   windSpeedElement.innerHTML = `${weatherData.data.wind.speed} km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${weatherData.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getWeatherForecast(weatherData.data.city);
 }
 
 function formatDate(date) {
@@ -40,11 +41,61 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function getWeatherForecast(city) {
+  let apiKey = "30927dtfa44b4770359oe8258a9c5b2c";
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios
+    .get(forecastUrl)
+    .then((response) => {
+      if (response.data.error) {
+        console.error("Error fetching forecast data");
+      } else {
+        updateForecast(response);
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("Error fetching forecast data. Please try again.");
+    });
+}
+
+function updateForecast(forecastData) {
+  let forecastContainer = document.querySelector("#forecast");
+  let forecast = forecastData.data.daily;
+
+  forecastContainer.innerHTML = "";
+
+  forecast.forEach((dayData, index) => {
+    if (index < 5) {
+      let dayElement = document.createElement("div");
+      dayElement.classList.add("weather-forecast-day");
+
+      let dayName = new Date(dayData.time * 1000).toLocaleString("en-us", {
+        weekday: "short",
+      });
+      let maxTemp = Math.round(dayData.temperature.max);
+      let minTemp = Math.round(dayData.temperature.min);
+      let iconUrl = dayData.condition.icon_url;
+
+      dayElement.innerHTML = `
+        <div class="weather-forecast-date">${dayName}</div>
+        <img src="${iconUrl}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature"><strong>${maxTemp}º</strong></div>
+          <div class="weather-forecast-temperature">${minTemp}º</div>
+        </div>
+      `;
+
+      forecastContainer.appendChild(dayElement);
+    }
+  });
+}
+
 function searchCity(city) {
   let apiKey = "30927dtfa44b4770359oe8258a9c5b2c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  // Show a loading state
   document.querySelector("#city").innerHTML = "Loading...";
   document.querySelector("#temperature").innerHTML = "...";
 
